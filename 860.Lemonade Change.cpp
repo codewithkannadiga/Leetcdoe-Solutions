4f7b650ee975efd07d2179d63d7a6d0bf2b505e4
codewithkannadiga <techkannada33@gmail.com>
2024-08-27 17:54:08 +0530

    Create 860.Lemonade Change.cpp

---
+++ 860.Lemonade Change.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fiveCount = 0;
+        int tenCount = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                fiveCount++;
+            } else if (bill == 10) {
+                if (fiveCount > 0) {
+                    fiveCount--;
+                    tenCount++;
+                } else {
+                    return false;
+                }
+            } else if (bill == 20) {
+                if (tenCount > 0 && fiveCount > 0) {
+                    tenCount--;
+                    fiveCount--;
+                } else if (fiveCount >= 3) {
+                    fiveCount -= 3;
+                } else {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+};
